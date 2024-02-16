df242af204a92073f239ba86d61f1a23321f570a
estiven <stivencv7@gmail.com>
2024-02-16 07:30:50 -0500

    Cuadrando el responsive

--- src/page/PageHome/PageHome.jsx
+++ src/page/PageHome/PageHome.jsx
@@ -6,7 +6,7 @@ import { SectionCryto } from "./SectionCryto/SectionCryto";
 
 export const PageHome = () => {
   return (
-    <div className=" bg-[#191E25] p-[36px] rounded-[32px] w-[52%] text-body ">
+    <div className=" bg-[#191E25] p-[36px] rounded-[32px] w-[52%] text-body h-[672px] ">
       <SectionNav />
       <div className="flex flex-col h-[538.74px] justify-between ">
         <SectionAccounts/>
